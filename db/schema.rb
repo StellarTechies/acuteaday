620928f1f80c1e6ca237884ed7ef451a44f5cca7
StellarTechies <tech@stellartechies.com>
2015-07-14 20:24:33 +0000

    strctre

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714131325) do
+ActiveRecord::Schema.define(version: 20150714201039) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "email"
@@ -62,5 +62,7 @@ ActiveRecord::Schema.define(version: 20150714131325) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "posts", ["author_id", "created_at"], name: "index_posts_on_author_id_and_created_at"
 
 end
