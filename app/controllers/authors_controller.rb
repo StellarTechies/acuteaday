544c84099707f262d5bf7b9012fa5f3cdc90ea68
StellarTechies <tech@stellartechies.com>
2015-07-14 17:28:12 +0000

    adding author form

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -69,6 +69,6 @@ class AuthorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def author_params
-      params[:author]
+      params.require(:author).permit(:name, :email, :password, :password_confirmation, :bio)
     end
 end
