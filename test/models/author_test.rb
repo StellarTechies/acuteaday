1a9bfce0503de42412faeb74f60e9264df56c173
StellarTechies <tech@stellartechies.com>
2015-07-15 18:58:57 +0000

    db migration and some controller code

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -4,4 +4,25 @@ class AuthorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @author = Author.new(name: "Brenda Segal", email: "bsegal26@gmail.com", bio: "all about me", image: "blah.jpg", password: "hmmmm2342", password_confirmation: "hmmmm2342")
+    @author_invalid = Author.new(name: "Brenda Segal", email: "bsegal26@gmail.com", bio: "all about me", image: "blah.jpg", password: "hmmm", password_confirmation: "hmmm")
+  end
+  
+  test "should be valid" do
+    assert @author.valid?
+  end
+  
+  test "should save author" do
+    assert @author.save
+  end
+  
+  test "should not be valid" do
+    assert_not @author_invalid.valid?
+  end
+  
+  test "should not save author" do
+    assert_not @author_invalid.save
+  end
 end
